54966b31c5dd5dabe917c13c9f00f679ff25dba5
Muradhanki <muradhankilinc@gmail.com>
2024-02-25 00:21:06 +0000

    Function to prompt for intern details

--- index.js
+++ index.js
@@ -95,3 +95,28 @@ function promptEngineer() {
       });
   }
   
+  // Function to prompt for intern details
+function promptIntern() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "name",
+          message: "Enter the intern's name:",
+        },
+        {
+          type: "input",
+          name: "id",
+          message: "Enter the intern's employee ID:",
+        },
+        {
+          type: "input",
+          name: "email",
+          message: "Enter the intern's email address:",
+        },
+        {
+          type: "input",
+          name: "school",
+          message: "Enter the intern's school:",
+        },
+      ])
